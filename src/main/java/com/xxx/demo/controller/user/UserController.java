9fbcb0c0fcd66886468209f242100fc0a48fb5c8
张葛 <helloworld_zac@163.com>
2017-11-29 23:46:05 +0800

    添加logback-spring.xml配置文件

--- src/main/java/com/xxx/demo/controller/user/UserController.java
+++ src/main/java/com/xxx/demo/controller/user/UserController.java
@@ -2,6 +2,8 @@ package com.xxx.demo.controller.user;
 
 import com.xxx.demo.models.sys.SysUser;
 import com.xxx.demo.services.user.SysUserServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping({"/user"})
 public class UserController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private SysUserServices sysUserServices;
 
@@ -22,7 +25,11 @@ public class UserController {
     @ResponseBody
     public String findById(String id) {
         SysUser user = sysUserServices.findById(id);
-
+        logger.trace("日志输出 trace");
+        logger.debug("日志输出 debug");
+        logger.info("日志输出 info");
+        logger.warn("日志输出 warn");
+        logger.error("日志输出 error");
         return "";
     }
 }
